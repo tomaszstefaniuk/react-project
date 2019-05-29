a8a7eedd1757124edd793861139b6d795bdb4d83
Tomek <esemku@icloud.com>
2019-05-29 22:08:44 +0200

    Adding Card.js and Icon.js

---
+++ src/components/Card/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+  }
+
+  render() {
+    return (
+      <section className={styles.component}>{this.props.cards}
+        <h3>{this.props.title}</h3>
+      </section>
+    )
+  }
+}
+
+export default Card;
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,52 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card.js';
+import {settings} from '../../data/dataStore.js';
+import Creator from '../Creator/Creator.js';
+import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
-  static propTypes = {
-    titleText: PropTypes.array,
+  state = {
+    cards: this.props.cards || [],
   }
+
+  static propTypes = {
+    title: PropTypes.string,
+    cards: PropTypes.array,
+  }
+
+  addCard(title){
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards[state.cards.length-1].key+1,
+            title,
+          }
+        ]
+      }
+    ));
+  }
+
 
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.titleText}</h3>
+        <h3 className={styles.title}>{this.props.title}
+          <span className={styles.icon}>
+            <Icon name={this.props.icon}/>
+          </span>
+        </h3>
+        <div>
+          {this.state.cards.map(({key, ...cardProps}) => (
+            <Card key={key} {...cardProps} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+        </div>
       </section>
     )
   }
