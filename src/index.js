43671bef4dee0b71a98b3d654791af5bca5e7e44
Tomek <esemku@icloud.com>
2019-06-01 22:20:08 +0200

    Add ESSLint and husky

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const test = "1"
 import React from 'react';
 import './styles/normalize.scss';
 import './styles/global.scss';
@@ -5,4 +6,3 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
